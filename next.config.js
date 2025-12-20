38308f5010a39a836cd8e8e80ef7f62bd2be715f
eros-codes <saghafiarvin@gmail.com>
2025-12-20 20:01:18 +0330

    cartfix31

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'utf-8-validate': require.resolve('./mocks/empty.js'),
+      'bufferutil': require.resolve('./mocks/empty.js'),
+    };
+    return config;
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
